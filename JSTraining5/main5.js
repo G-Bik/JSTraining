69f4cb47b300fd27f6a7c0bdfb247cdb8d690d37
Gustut Biksa <gustutbiksa@gmail.com>
2025-06-19 21:43:36 +0800

    commit Backup

---
+++ JSTraining5/main5.js
@@ -0,0 +1,9 @@
+/**
+ BELAJAR PERULANGAN di JAVASCRIPT
+ for, for of, for in, foreach
+ while & do while
+ */
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+}
