4834c2959283cbc64f090a578fbc30b915d11ba5
G-Bik <gustutbiksa@gmail.com>
2025-06-20 10:03:42 +0800

    Belajar array yang berisi function dan mengelolanya

--- JSTraining5/main5.js
+++ JSTraining5/main5.js
@@ -4,6 +4,28 @@
  while & do while
  */
 
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
+data = [
+  "saya",
+  4,
+  5,
+  "mungkin saja",
+  function arrayFunction(...parrams) {
+    parrams[0] = console.log("kamu melakukan print fungsi dalam array");
+    parrams[1] = console.log(
+      "Oke sekarang kita melakukan display parameter kedua"
+    );
+    return parrams[2] + parrams[3];
+  },
+  "yuk semangat kerja",
+  18,
+  2025,
+];
+
+for (let index = 0; index < data.length; index++) {
+  if (typeof data[index] === "function") {
+    // Menangkap jenis data array berupa function dengan operator typeof
+    console.log(data[4](null, null, 45, 46));
+  }
+
+  console.log(typeof data[index]);
 }
