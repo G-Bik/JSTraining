5e9e2efa54bf958b25b9f9e6e789fc193a65ec5b
G-Bik <gustutbiksa@gmail.com>
2025-06-23 11:26:03 +0800

    memecahkan permasalahan for dalam for dan bug pada array data karena row function yang tidak beropbjek

--- JSTraining5/main5.js
+++ JSTraining5/main5.js
@@ -41,33 +41,34 @@ for (let index = 0; index < data.length; index++) {
     // melakukan push namma fungsi dengan yang memiliki format standar (yang memiliki nama) ke dalam arary yang baru (funcToObject) agar nantinya array baru tersebut menyimpan nama-nama fungsi standar yang akan dipanggil pada if else secara dinamis dengan data array lama (data)
     funcToObject.push(identitasFungsi);
 
-    console.log(funcToObject);
+    console.log(`print FunctoObject ${funcToObject}`);
 
     // Melakukan for pada array baru (funcToObject) yang berisikan list nama-nama fungsi dan dibandingkan dengan array lama (data) dengan property .name untuk mengambil nama fungsi dari array lama tersebut(data)
     for (let index1 = 0; index1 < funcToObject.length; index1++) {
+      console.log(index1);
+
       // Membandingkan apakah nama fungsi di array lama sama dengan array baru
+
+      /** Sebenarnya terdapat masalah pada else if disini, yang mana else if selalu bernilai true karena dia membandingkan nilai funcToObject yang mana bervalue array dengan masking nama2 function pada array data. nah karena jenis functionnya adalah arrow function yang tidak memiliki idnentitas, maka funcToObject dibandingkan dengan data[index].name yang mana membandingkan nama yang tidak sama, maka dari itu nilai else if selalu menjadi true
+       
+       nah diluar hal tersebut, mengapa output else if ini bisa berjalan 2 kali jika tidak menggunakan break;, alasan utamanya adalah for dalam for, nah karena index1 (for dalam) merupakan value dari index (for luar), maka disetiap perulanngan index (for luar) akan terus mengulang secara total lagi index1 (for dalam),
+
+       nah dalam kasus kode javascript ini, karena ditemukan 2 function pada array data, maka perulangan index akan terjadi 2 kali, nah dalam 2 kali tersebut index1 akan terus berulang total data funcToObject disetiap perulangan index. maka dari itu kondisi else if akan terus masuk.
+
+       nah semua bug ini terjadi karena jenis data arrow function tidak di objek kan pada array data, yang mana seharusnya jika arrow function di objek kan pada array data maka pengkondisian if -else pada index1 akan lebih tepat tanpa menyebabkan bug seperti ini.
+      */
       if (funcToObject[index1] === data[index].name) {
         hasilfungsi1 = data[index](null, null, 10, 25);
         console.log(`Berhasil madafaka : ${hasilfungsi1}`);
-      } else if (data[index].name == "") {
-        // IYAKKK TERNYATA INI KONDISINYA MASIH TERBACA 2 BUNGGG, AYO PERBAIKI YANG INI!!!
+      } else if (funcToObject[index1] != data[index].name) {
         hasilfungsi2 = data[index](Math.random() * 1000, Math.random() * 1000);
         console.log(`MASUK KE FUNGSI 2 MADAFAKA ${hasilfungsi2}`);
+        break;
       } else {
         console.log("tidak masuk if else");
       }
     }
   }
 }
-
-// Contoh menangkap function dalam array dan dilakukan pada iterasi data array itu sendiri dalam for
-// for (let index = 0; index < data.length; index++) {
-//   if (typeof data[index] === "function") {
-//     // Menangkap jenis data array berupa function dengan operator typeof, yang mana indikator value utk menangkap sebuah fungsi adalah "function"
-//     console.log(data[4](null, null, 45, 46));
-//   }
-
-//   console.log(data[index]);
-// }
 
 // Melakukan optimasi refactor if-else dengan menggunakan map + workrround dan switch-case
